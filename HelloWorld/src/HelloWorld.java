da50d24dd8087432a2ed97b164df6c49cfb03eab
sahaj <sahaj.kaur1@ucalgary.ca>
2026-06-17 01:01:34 -0600

    added comments

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -4,11 +4,13 @@ import java.util.Scanner;
 
 public class HelloWorld {
 
+    // Method for when the user chooses dog
     static void Dog(){
 
         System.out.println("Woof!Woof!");
     }
 
+    // Method for when the user chooses cat
     static void Cat(){
         System.out.println("meoww meowww");
     }
@@ -17,12 +19,16 @@ public class HelloWorld {
     {
         Scanner scan = new Scanner (System.in);
 
+        // Welcome message
         System.out.println("Welcome to the Pet Simulator!! A place where you can take care of a pet without all the mess!");
 
+        // Loops until the user inputs a valid response
         while(true) {
+            // Asking the user dog/cat
             System.out.println("Please choose your pet- Dog/Cat");
             String pet = scan.nextLine().toLowerCase();
 
+            // Handling their input
             if (Objects.equals(pet, "dog")) {
             System.out.println("A dog! Good choice!");
             Dog();
