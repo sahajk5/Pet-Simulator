f5cd17ffd938ecd5858f0fec97975cdc15cf8c25
sahaj <sahaj.kaur1@ucalgary.ca>
2026-06-21 19:38:47 -0600

    completed Cat()

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -189,6 +189,141 @@ public class HelloWorld
         System.out.println("Meoww meowww");
         System.out.println("This is your cat "+catName+".");
         System.out.println("Now your job is to take care of it and make sure it doesn't die!");
+
+        // Variables for the cat's needs
+        int happy = 10;
+        int hunger = 10;
+        int energy = 10;
+
+        System.out.println("Right now " + catName + "'s needs are at these levels-\n" +
+                "Happiness = " + happy + "\n" +
+                "Hunger = " + hunger + "\n" +
+                "Energy = " + energy);
+
+
+        while (true)
+        {
+            System.out.println("What's your next action? (play/eat/sleep/exit)");
+            String action = scan.nextLine().toLowerCase();
+
+            // When the user selects play
+            if (action.equals("play")) {
+                System.out.println("               )\\._.,--....,'``.      \n" +
+                        " .b--.        /;   _.. \\   _\\  (`._ ,.\n" +
+                        "`=,-,-'~~~   `----(,_..'--(,_..'`-.;.'");
+                System.out.println("Looks like "+catName+" found a new friend to play with!");
+
+                // Update the stats after playing
+                if (happy < 10) {
+                    happy = happy + 2;
+                    if (happy > 10) {
+                        happy = 10;
+                    }
+                }
+
+                if (hunger > 0) {
+                    hunger = hunger - 3;
+                    if (hunger < 0) {
+                        hunger = 0;
+                    }
+                }
+
+                if (energy > 0) {
+                    energy = energy - 2;
+                    if (energy < 0) {
+                        energy = 0;
+                    }
+                }
+            }
+
+            // When the user selects eat
+            else if (action.equals("eat")) {
+                System.out.println("                                _.---.\n" +
+                        "                      |\\---/|  / ) cat|\n" +
+                        "          ------------;     |-/ /|food|---\n" +
+                        "                      )     (' / `---'\n" +
+                        "          ===========(       ,'==========\n" +
+                        "          ||   _     |      |\n" +
+                        "          || o/ )    |      | o\n" +
+                        "          || ( (    /       ;\n" +
+                        "          ||  \\ `._/       /\n" +
+                        "          ||   `._        /|\n" +
+                        "          ||      |\\    _/||\n" +
+                        "        __||_____.' )  |__||____________\n" +
+                        "         ________\\  |  |_________________\n" +
+                        "                  \\ \\  `-.\n" +
+                        "                   `-`---'  ");
+                System.out.println("LOL I guess "+catName+" will get its food itself");
+
+                // Update stats after eating
+                if (happy > 0) {
+                    happy = happy - 1;
+                }
+
+                if (hunger < 10) {
+                    hunger = hunger + 3;
+                    if (hunger > 10) {
+                        hunger = 10;
+                    }
+                }
+
+                if (energy < 10) {
+                    energy = energy + 2;
+                    if (energy > 10) {
+                        energy = 10;
+                    }
+                }
+            }
+
+            // When the user selects sleep
+            else if (action.equals("sleep"))
+            {
+                System.out.println("   |\\      _,,,---,,_\n" +
+                        "   /,`.-'`'    -.  ;-;;,_\n" +
+                        "  |,4-  ) )-,_..;\\ (  `'-'\n" +
+                        " '---''(_/--'  `-'\\_) ");
+                System.out.println("Awww it looks so peaceful sleeping");
+
+                // Update stats after sleeping
+                if (happy > 0) {
+                    happy = happy - 1;
+                }
+
+                if (hunger > 0) {
+                    hunger = hunger - 5;
+                    if (hunger < 0) {
+                        hunger = 0;
+                    }
+                }
+
+                if (energy < 10) {
+                    energy = energy + 3;
+                    if (energy > 10) {
+                        energy = 10;
+                    }
+                }
+            }
+
+            // When the user selects exit
+            else if (action.equals("exit")){
+                break; // ends the loop
+            }
+
+            // When the user puts anything else other than the options given
+            else
+            {
+                System.out.println("Please choose a correct action.");
+            }
+
+            // This shows the user the lastest stats of their cat
+            if (action.equals("play") || action.equals("eat") || action.equals("sleep")){
+                System.out.println("These are the current stats now-\n" +
+                        "Happiness = " + happy + "\n" +
+                        "Hunger = " + hunger + "\n" +
+                        "Energy = " + energy);
+            }
+
+        }
     }
 
     public static void main (String[] args)
